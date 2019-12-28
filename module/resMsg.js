e2ea9dad890e25aaa2ebf9f043ff5a04bc60a444
gngsn <rudtjs4540@gmail.com>
2019-12-28 21:55:30 +0900

    update response message

--- module/resMsg.js
+++ module/resMsg.js
@@ -1,6 +1,13 @@
 module.exports = {
-    NULL_VALUE : '입력되지 않은 값이 있습니다',
-    FIND_PATH_COMPLETE : '경로 찾기 성공',
-    FIND_PATH_FAILED : '경로 찾기 실패',
-    ADD_SCHEDULE_SUCCESS : '일정 추가 성공'
+    NULL_VALUE: '입력되지 않은 값이 있습니다',
+    FIND_PATH_COMPLETE: '경로 찾기 성공',
+    FIND_PATH_FAILED: '경로 찾기 실패',
+    ADD_SCHEDULE_SUCCESS: '일정 추가 성공',
+
+    // 알림 전송 메세지
+    SEND_MESSAGE_SUCCESS: '알림 전송에 성공하였습니다.',
+    SEND_MESSAGE_FALSE: '알림 전송에 실패하였습니다.',
+    WRONG_FLAG: 'alarmFlag가 범위에서 벗어났습니다.',
+
+    SERVER_ERROR: '서버 내부 오류입니다.'
 }
