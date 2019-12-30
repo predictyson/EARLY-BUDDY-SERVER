860c4d654d9cf8bd6e2f87db596c15eaccc4c425
SimJungUk <simju9397@gmail.com>
2019-12-30 23:43:02 +0900

    [Add standard to searchPath ]

--- module/odsayAPI.js
+++ module/odsayAPI.js
@@ -2,10 +2,10 @@ const request = require('request');
 const ak = require('../config/appkey');
 
 module.exports = {
-    searchPubTransPath: (SX, SY, EX, EY) => {
+    searchPubTransPath: (SX, SY, EX, EY, category) => {
         return new Promise((resolve, reject) => {
             const options = {
-                'uri': `https://api.odsay.com/v1/api/searchPubTransPathR?apiKey=${ak.odsay}&SX=${SX}&SY=${SY}&EX=${EX}&EY=${EY}`,
+                'uri': `https://api.odsay.com/v1/api/searchPubTransPathR?apiKey=${ak.odsay}&SX=${SX}&SY=${SY}&EX=${EX}&EY=${EY}&category=${category}`,
             }
             request(options, (err, result) => {
                 if (err) reject (err);
